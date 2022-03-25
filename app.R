63fbd58a6bf9b885c57868c35040760a80d1cdd2
seehorne <95896637+seehorne@users.noreply.github.com>
2022-03-24 22:38:38 -0400

    #externship updates to app.R
    # added legend to chart
    # added y axis category labels to chart
    # made chart be able to display based on percentages
    # added color scheme and removed borders to chart
    # added percentage vs counts toggle for chart display
    # changed what happens when states AL-CT are selected (chart is now produced as opposed to error)
    
    #lots of things, such as the beginnings of a reframe to work more easily with ggplot instead of base r are left throughout as comments

--- app.R
+++ app.R
@@ -8,18 +8,18 @@ library(shiny)
 
 ui <- (fluidPage(
   titlePanel("Food Security"),
-    sidebarPanel(
-      #Making filter choice bar
+  sidebarPanel(
+    #Making filter choice bar
     selectInput("filters","Select a Filter", c("None",
-                                            "Citizenship",
-                                            "Disability",
-                                            "Education",
-                                            "Food Stamps",
-                                            "Income",
-                                            "Industry",
-                                            "Number of Jobs",
-                                            "Race",
-                                            "Sex"), 
+                                               "Citizenship",
+                                               "Disability",
+                                               "Education",
+                                               "Food Stamps",
+                                               "Income",
+                                               "Industry",
+                                               "Number of Jobs",
+                                               "Race",
+                                               "Sex"), 
                 selected = "None"), #Default is no filters
     #Making state choice bar
     selectInput("states", "Select a State", c("All",
@@ -74,12 +74,17 @@ ui <- (fluidPage(
                                               "WV",
                                               "WI",
                                               "WY"), 
-                selected = "All")), #Default is national-level view
+                selected = "All"),#Default is national-level view
+    selectInput("display", "Display Type", c("None Selected",
+                                             "Percentage", 
+                                             "Counts"),
+                selected = "None Selected"),),#default is no view (no chart)
+  
   
   mainPanel(
     plotOutput("plot")
-    )
-  ))
+  )
+))
 
 # Server
 
@@ -90,106 +95,160 @@ server <- (function(input, output){
     
     #Filters by case
     if(input$filters == "Citizenship"){
+      #useLabels <- unique(g)
+      #categories <- useLabels$Cit
+      #print(categories)
       data <- select(data, FSecurity, Cit)
     }
     
     if(input$filters == "Disability"){
+      #useLabels <- unique(h)
+      #categories <- useLabels$Dis
+      #print(categories)
       data <- select(data, FSecurity, Dis)
     }
     
     if(input$filters == "Education"){
+      #useLabels <- unique(c)
+      #categories <- useLabels$edu
+      #print(categories)
       data <- select(data, FSecurity, edu)
     }
     
     if(input$filters == "Food Stamps"){
+      #useLabels <- unique(j)
+      #categories <- useLabels$Food
+      #print(categories)
       data <- select(data, FSecurity, Food)
     }
     
     if(input$filters == "Income"){
+      #useLabels <- unique(a)
+      #categories <- useLabels$inc
+      #print(categories)
       data <- select(data, FSecurity, inc)
     }
     
     if(input$filters == "Industry"){
+      #useLabels <- unique(i)
+      #categories <- useLabels$Ind
+      #print(categories)
       data <- select(data, FSecurity, Ind)
     }
     
     if(input$filters == "Number of Jobs"){
+      #useLabels <- unique(f)
+      #categories <- useLabels$jobs
+      #print(categories)
       data <- select(data, FSecurity, jobs)
     }
     
     if(input$filters == "Race"){
+      #these were used to try to get categories to work in ggplot
+      #useLabels <- unique(e)
+      #categories <- useLabels$races
+      #print(categories)
+      
       data <- select(data, FSecurity, races)
+      
+      #these were used to investigate why certain bars didn't exist
+      #number <- count(data, races, FSecurity)
+      #print(as_tibble(number), n=100)
     }
     
     if(input$filters == "Sex"){
+      #useLabels <- unique(d)
+      #categories <- useLabels$sexes
+      #print(categories)
       data <- select(data, FSecurity, sexes)
     }
-
-#---------------------------------------------
     
+    #---------------------------------------------
     #States
     #AL-CT are all missing any entries so they just get a missing message.
+    # ^no they aren't single digits was just reading in weird
     if(input$states == "AL"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Alabama."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      #num <- count(State,states)
+      #print(num)
+      #print(State)
+      #test <- count(data,states)
+      #print(test)
+      
+      temp <- filter(data, states == "AL")
+      data <- select(temp, FSecurity, states)
+      
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Alabama."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      #))
     }
     
     if(input$states == "AK"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Alaska."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Alaska."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "AK")
+      data <- select(temp, FSecurity, states)
     }
     
     if(input$states == "AZ"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Arizona."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Arizona."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "AZ")
+      data <- select(temp, FSecurity, states)
     }
     
     if(input$states == "AR"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Arkansas."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Arkansas."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "AR")
+      data <- select(temp, FSecurity, states)
     }
     
     if(input$states == "CA"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from California."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from California."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "CA")
+      data <- select(temp, FSecurity, states)
     }
     
     if(input$states == "CO"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Colorado."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Colorado."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "CO")
+      data <- select(temp, FSecurity, states)
     }
     
     if(input$states == "CT"){
-      showModal(modalDialog(
-        title = "Missing Data",
-        paste0("No available data from Connecticut."),
-        easyClose = TRUE,
-        footer = NULL
-      ))
+      # showModal(modalDialog(
+      #   title = "Missing Data",
+      #   paste0("No available data from Connecticut."),
+      #   easyClose = TRUE,
+      #   footer = NULL
+      # ))
+      temp <- filter(data, states == "CT")
+      data <- select(temp, FSecurity, states)
     }
     
     #The rest of the states actually have data so they get real filters.
@@ -409,10 +468,64 @@ server <- (function(input, output){
       data <- select(temp, FSecurity, states)
     }
     
-#--------------------------------------------
+    #--------------------------------------------
+    #legend strategy from: 
+    # https://statisticsglobe.com/move-position-of-barplot-legend-in-r
+    #axis labels strategy from:
+    # https://statisticsglobe.com/rotate-axis-labels-in-r 
+    #percentage creation strategy from:
+    # https://r-graph-gallery.com/211-basic-grouped-or-stacked-barplot.html
     
-   barplot(table(data)) #Making actual plot
-   
-})})
+    if (input$display == "Percentage"){
+      data_percentage <- apply(table(data), 2, function(x){x*100/sum(x,na.rm=T)})
+      par(mar=c(10,4,4,10), xpd=TRUE) #reset margins to make text/legend fit
+      #bottom left top right
+      barplot(data_percentage,#Making actual plot
+              col = 3:nrow(data),#the number in front sets a pallette
+              border= NA,
+              las = 2, #sets angling, must be whole number
+              cex.names = 0.5, #text size for axes labels
+              legend.text = TRUE,
+              args.legend = list(x = "right",
+                                 inset = c(-0.4,0))) #positions legend 
+      title(main = input$filters)
+    }
+    if (input$display == "Counts"){
+      par(mar=c(10,4,4,10), xpd=TRUE) #reset margins to make text/legend fit
+      #bottom left top right
+      barplot(table(data),#Making actual plot
+              col = 3:nrow(data),#the number in front sets a pallette
+              border= NA,
+              las = 2, #sets angling, must be whole number
+              cex.names = 0.5, #text size for axes labels
+              legend.text = TRUE,
+              args.legend = list(x = "right",
+                                 inset = c(-0.4,0))) #positions legend 
+      title(main = input$filters)
+    }
+    
+    
+    #start for ggplot code, not super useful with current data structure
+    #ashes_df %>% #commented out bc it
+    
+    # count(data,input$filters,FSecurity)  %>% 
+    #   group_by(input$filters)  %>%#group by first thing?
+    #   mutate(pct= prop.table(n) * 100)  %>% 
+    # ggplot()+ 
+    #   print (aes(x=categories, y=pct,
+    #               #needs to be filter category relative to FSecurity                  
+    #              fill=FSecurity)) + 
+    #   geom_bar(position="fill", stat="identity")
+    
+    
+    
+    # geom_bar(stat="identity") +
+    # ylab("Number of Participants") +
+    # geom_text(aes(label=paste0(sprintf("%1.1f", pct),"%")),
+    #           position=position_stack(vjust=0.5)) +
+    # ggtitle("England & Australia Team Make Up") +
+    # theme_bw()
+    
+  })})
 
 shinyApp(ui = ui, server = server)
